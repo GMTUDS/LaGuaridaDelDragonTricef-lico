92cb880459424c9dea94b4598f1625f98bdb69a3
GMTUDS <142052001+GMTUDS@users.noreply.github.com>
2024-06-08 05:52:05 -0300

    svdvsd

--- src/LaGuaridadelDragon/NivelTesoro.java
+++ src/LaGuaridadelDragon/NivelTesoro.java
@@ -1,24 +1,4 @@
 package LaGuaridadelDragon;
-/**
- *
- * @author nn
- */
-public class NivelTesoro {
-    private Tesoro tesoro;
-    private int nivel;
 
-    public NivelTesoro(Tesoro tesoro, int nivel) {
-        this.tesoro = tesoro;
-        this.nivel = nivel;
-    }
-
-    public Tesoro getTesoro() {
-        return tesoro;
-    }
-
-    public int getNivel() {
-        return nivel;
-    }
-}
 
 
--- src/LaGuaridadelDragon/Nodo.java
+++ src/LaGuaridadelDragon/Nodo.java
@@ -1,17 +1,2 @@
-<<<<<<< Updated upstream
-package LaGuaridadelDragon;
-/**
- *
- * @author nn
- */
-public class Nodo {
-    private Tesoro tesoro;
-    private Nodo izquierdo;
-    private Nodo central;
-    private Nodo derecho;
-=======
 
 package LaGuaridadelDragon;
-
->>>>>>> Stashed changes
-
